c530b6f609348d5b1c1fe3b3b1514159daa334d0
tre-over <ntresorolivier@gmail.com>
2022-11-29 20:32:34 +0100

    Ajoutez des fichiers projet.

---
+++ LoginGettingStarted/LoginGettingStarted/LoginUI.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginGettingStarted
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+        private void button_clicked(object sender, EventArgs e)
+        {
+            if(txtUsername.Text=="admin" && txtPassword.Text == "123")
+            {
+                Navigation.PushAsync(new Home());
+            }
+            else
+            {
+                DisplayAlert("ops...,Username or Password is incorrect");
+            }
+        }
+
+        private void DisplayAlert(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registerpage());
+        }
+
+       
+    }
+}
